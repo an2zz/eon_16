1698496acaf5831ee096c4c198af0f1c6a09d026
an2zz <so_cooll_@naver.com>
2020-06-23 16:01:27 +0900

    0521

--- snail.py
+++ snail.py
@@ -1,10 +1,10 @@
-num_list = [[0] * 6 for i in range(6)]  
+num = int(input('수를 입력하세요 : '))
+num_list = [[0] * 6 for i in range(num)]  
 a = 0
 b= 1
 i = 0
 j = -1
-num = 6
- 
+
 while True:
     for p in range(1, num + 1):
         a += 1
@@ -22,5 +22,5 @@ while True:
         
 for i in range(len(num_list)):
     for j in range(len(num_list[0])):
-        print('%3d ' % num_list[i][j], end = '')
+        print('%d ' % num_list[i][j], end = '')
     print()
